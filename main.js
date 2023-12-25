31cefba16c531dea1ed098b6a76e36e924556bb1
Shreyansh Agarwal <96068205+Shreyanshgarwal@users.noreply.github.com>
2023-12-25 13:31:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0
+noseY=0
+function preload() {  
+   mustache=loadImage(' https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup() {
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw() {    
+    image(video,0,0,300,300);
+    image(mustache,noseX,noseY,30,30);
+    stroke(255,0,0);
+    fill(255,0,0)
+}
+
+function take_snapshot() {   
+     save('myFilterImage.png'); 
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initalized');
+}
+
+function gotPoses(results) {
+   if (results.length > 0) {
+    console.log(results);
+    noseX=results[0].pose.nose.x - 15;
+    noseY=results[0].pose.nose.y + 5;
+    console.log("nose x =" + results[0].pose.nose.x);
+    console.log("nose y =" + results[0].pose.nose.y);
+    console.log("nose x="+noseX);
+    console.log("nose y="+noseY);
+   }
+}
+
